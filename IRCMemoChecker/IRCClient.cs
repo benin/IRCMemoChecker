617c3834e710a9d289b2a39cac4226eb79edfea7
Vladimir Karlov <bbenin@gmail.com>
2014-11-27 12:44:39 +0200

    Initial IRCMemoChecker commit. Basic implementation of IRC connection.

---
+++ IRCMemoChecker/IRCClient.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace IRCMemoChecker
+{
+    class IRCClient
+    {
+        // Basic notification for owner
+        public delegate void NotificationContainer(string strMessage, NotifyType type);
+        public delegate void ConnectionChanged(ConnectionStateType type);
+
+        public event NotificationContainer OnNotification;
+        public event ConnectionChanged OnConnectionChanged;
+
+        public bool IsConnected { get { return ConnectionState == ConnectionStateType.Connected; } }
+
+        public ConnectionStateType ConnectionState
+        {
+            get { return connState; }
+            set
+            {
+                connState = value;
+                if (OnConnectionChanged != null)
+                    OnConnectionChanged(connState);
+            }
+        }
+        private ConnectionStateType connState = ConnectionStateType.Disconnected;
+
+        // client data
+        private StreamSocket clientSocket = null;
+        CancellationToken tokenCancel;
+        
+        private DataWriter dataWriter = null;
+        private DataReader dataReader = null;
+
+        CoreDispatcher parentDispatcher;
+
+        private string strPrefDataBuffer = string.Empty;
+
+        // Notification
+        public enum NotifyType
+        {
+            DataMessage,
+            StatusMessage,
+            ErrorMessage
+        };
+        
+        public enum ConnectionStateType
+        {
+            Disconnected,
+            Connecting,
+            Connected
+        }
+
+        public IRCClient(CoreDispatcher dispatcher)
+        {
+            parentDispatcher = dispatcher;
+            OnConnectionChanged += OnClientConnectionChanged;
+        }
+
+        private void Notify(string strMessage, NotifyType type)
+        {
+            if (OnNotification != null)
+            {
+                var ignore = parentDispatcher.RunAsync(
+                    CoreDispatcherPriority.Normal, () => OnNotification(strMessage, type));
+            }
+        }
+
+        public async void Connect(Uri uri)
+        {
+            if (ConnectionState != ConnectionStateType.Disconnected)
+            {
+
+                switch (ConnectionState)
+                {
+                    case ConnectionStateType.Connecting:
+                        Notify("Connecting...", NotifyType.ErrorMessage);
+                        break;
+                    case ConnectionStateType.Connected:
+                        Notify("Already connected.", NotifyType.ErrorMessage);
+                        break;
+                    default:
+                        break;
+                }
+                return;
+            }
+
+            try
+            {
+                Notify("Trying to connect ...", NotifyType.StatusMessage);
+
+                if (clientSocket == null)
+                    clientSocket = new StreamSocket();
+
+                ConnectionState = ConnectionStateType.Connecting;
+
+                // Set timeout 3 sec
+                CancellationTokenSource canclConnect = new CancellationTokenSource();
+                canclConnect.CancelAfter(1000);
+                tokenCancel = canclConnect.Token;
+
+                // Try to connect to the 
+                await clientSocket.ConnectAsync(new HostName(uri.Host), uri.Port.ToString(),
+                    SocketProtectionLevel.PlainSocket).AsTask(canclConnect.Token);
+
+                ConnectionState = ConnectionStateType.Connected;
+
+                Notify("Connection established", NotifyType.StatusMessage);
+                
+            }
+            catch (TaskCanceledException exception)
+            {
+                Notify("Connection timedout: " + exception.Message, NotifyType.ErrorMessage);
+
+                // the Close method is mapped to the C# Dispose
+                if (clientSocket != null)
+                    clientSocket.Dispose();
+                clientSocket = null;
+
+                ConnectionState = ConnectionStateType.Disconnected;
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status, 
+                // it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                Notify("Connect failed with error: " + exception.Message, NotifyType.ErrorMessage);
+
+                // the Close method is mapped to the C# Dispose
+                if (clientSocket != null)
+                    clientSocket.Dispose();
+                clientSocket = null;
+
+                ConnectionState = ConnectionStateType.Disconnected;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (ConnectionState == ConnectionStateType.Disconnected|| clientSocket == null)
+            {
+                Notify("Not connected.", NotifyType.ErrorMessage);
+                return;
+            }
+
+            try
+            {
+                Notify("Disconnecting...", NotifyType.StatusMessage);
+
+                if (ConnectionState == ConnectionStateType.Connected)
+                {
+                    Send("QUIT buy buy");
+                }
+
+                if (ConnectionState == ConnectionStateType.Connecting)
+                    tokenCancel.ThrowIfCancellationRequested();
+
+                // Try to connect to the 
+                clientSocket.Dispose();
+                clientSocket = null;
+
+                ConnectionState = ConnectionStateType.Disconnected;
+
+                Notify("Connection closed", NotifyType.StatusMessage);
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status, 
+                // it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                Notify("Disconnect failed with error: " + exception.Message, NotifyType.ErrorMessage);
+
+                // the Close method is mapped to the C# Dispose
+                clientSocket = null;
+            }
+        }
+
+        private async void OnClientConnectionChanged(ConnectionStateType type)
+        {
+            switch (type)
+            {
+                case ConnectionStateType.Connected:
+                    dataReader = new DataReader(clientSocket.InputStream);
+                    dataReader.InputStreamOptions = InputStreamOptions.Partial;
+                    dataReader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                    dataReader.ByteOrder = Windows.Storage.Streams.ByteOrder.LittleEndian;
+
+                    //canclRead = new CancellationTokenSource();
+
+                    dataWriter = new DataWriter(clientSocket.OutputStream);
+
+                    Handshake();
+
+                    if (parentDispatcher != null)
+                        await parentDispatcher.RunAsync(CoreDispatcherPriority.Normal, () => OnListen());
+
+                    break;
+                case ConnectionStateType.Disconnected:
+                    //canclRead.Cancel();
+
+                    //canclRead.Dispose();
+                    dataWriter = null;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Handshake()
+        {
+            Send("NICK IRCClient");
+            Send("USER IRCClient 0 * :IRCClient");
+        }
+
+        private async void OnListen()
+        {
+            if (IsConnected && clientSocket != null)
+            {
+                bool listen = true;
+                while (listen)
+                {
+                    // Reading data from server
+                    string receivedData;
+
+                    try
+                    {
+                        var count = await dataReader.LoadAsync(1024);
+
+                        if (count > 0)
+                        {
+                            receivedData = dataReader.ReadString(count);
+
+                            // Divide onto lines
+                            FormatData(receivedData);
+
+                            //Notify(receivedData, NotifyType.DataMessage);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        listen = false;
+                    }
+                }
+                
+            }
+        }
+
+        // Format data, split on lines
+        private void FormatData(string strData)
+        {
+            // split the data into parts
+            string[] ircData = strData.Split(new string[] { "\n", "\r\n" }, 
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Check endline, if not - wait for next data
+            string strTempBuf = string.Empty;
+            if (!strData.EndsWith(Environment.NewLine))
+            {
+                strTempBuf = ircData[ircData.Length - 1];
+                Array.Clear(ircData, ircData.Length - 1, 1);
+            }
+            
+            foreach (string s in ircData)
+            {
+                if (String.IsNullOrEmpty(s))
+                    continue;
+
+                if (!String.IsNullOrEmpty(strPrefDataBuffer))
+                {
+                    ParseData(strPrefDataBuffer + s);
+                    strPrefDataBuffer = string.Empty;
+                    continue;
+                }
+
+                ParseData(s);
+            }
+
+            if (strTempBuf != string.Empty)
+                strPrefDataBuffer = strTempBuf;
+        }
+
+        // Parse lined data
+        private void ParseData(string strData)
+        {
+            Notify(strData, NotifyType.DataMessage);
+
+            // split the data into parts
+            string[] ircData = strData.Split(' ');
+
+            // if the message starts with PING we must PONG back
+            if (strData.Length > 4)
+            {
+                if (strData.Substring(0, 4) == "PING")
+                {
+                    Send("PONG " + ircData[1]);
+                    return;
+                }
+            }
+        }
+
+        public async void Send(string sendString)
+        {
+            if (!IsConnected)
+            {
+                Notify("Is not connected!", NotifyType.ErrorMessage);
+                return;
+            }
+
+            // Writing data to the writer will just store data in memory.
+            dataWriter.WriteString(sendString + Environment.NewLine);
+
+            try
+            {
+                // Call StoreAsync method to store the data to a backing stream
+                await dataWriter.StoreAsync();
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status, 
+                // it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                Notify("Send data or receive failed with error: " + exception.Message, NotifyType.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ IRCMemoChecker/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+using NotifyType = IRCMemoChecker.IRCClient.NotifyType;
+
+namespace IRCMemoChecker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private IRCClient ircClient;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            ircClient = new IRCClient(this.Dispatcher);
+            ircClient.OnNotification += NotifyUser;
+            ircClient.OnConnectionChanged += OnConnectionStateChanged;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            switch (ircClient.ConnectionState)
+            {
+                case IRCClient.ConnectionStateType.Disconnected:
+                    // Start connecting
+                    ircClient.Connect(new Uri(ServerHostname.Text));
+                    break;
+                case IRCClient.ConnectionStateType.Connecting:
+                case IRCClient.ConnectionStateType.Connected:
+                    ircClient.Disconnect();
+                    break;
+                default:
+                    break;
+            }    
+        }
+
+        private void OnConnectionStateChanged(IRCClient.ConnectionStateType type)
+        {
+            switch (type)
+            {
+                case IRCClient.ConnectionStateType.Disconnected:
+                    ConnectButton.Content = "Connect";
+                    break;
+                case IRCClient.ConnectionStateType.Connecting:
+                    ConnectButton.Content = "Cancel";
+                    break;
+                case IRCClient.ConnectionStateType.Connected:
+                    ConnectButton.Content = "Disconnect";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            ircClient.Send(SendText.Text);
+        }
+
+        private void NotifyUser(string strMessage, NotifyType type)
+        {
+            if (type == NotifyType.DataMessage)
+            {
+                OutputLog.Text += strMessage + Environment.NewLine;
+
+                // Scroll to view data
+                OutputLogViewer.UpdateLayout();
+                OutputLogViewer.ChangeView(null, OutputLogViewer.ScrollableHeight, null);
+            }
+            else if (StatusBlock != null)
+            {
+                switch (type)
+                {
+                    case NotifyType.StatusMessage:
+                        StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                        break;
+                    case NotifyType.ErrorMessage:
+                        StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                        break;
+                }
+                StatusBlock.Text = strMessage;
+
+                // Collapse the StatusBlock if it has no text to conserve real estate.
+                if (StatusBlock.Text != String.Empty)
+                {
+                    StatusBorder.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    StatusBorder.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void StatusBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            StatusBorder.Visibility = Visibility.Collapsed;
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void AppBarLogButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            OutputLog.Visibility = ButtonLog.IsChecked == true ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
